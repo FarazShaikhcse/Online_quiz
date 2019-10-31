cb268967ee43380a2f4ba5bfad2048fe059d96f1
KarthikUKumar <55427841+KarthikUKumar@users.noreply.github.com>
2019-10-31 22:21:03 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "webproject";
+$q=$_POST['mark'];
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO ATTEMPT (Userid,Examid,Mark)
+VALUES (20190501,1900,$q)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Your marks has been recorded";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
